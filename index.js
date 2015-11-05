b63acb917f64ec8c41be1ec3c2c0806c7b20fc34
freejacque <freejacque@gmail.com>
2015-11-05 18:34:38 -0500

    added a comment

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var mysql = require('mysql'),
     http  = require('http'),
     url   = require('url'),
     querystring = require('querystring');
+
+
+// Start a web server on port 8888. Requests go to function handleRequest
+
